6d5f0d54edb8c04782d9e61e3a370d7ecfbb7b7d
Kaitlyn <ksmetler@gmail.com>
2025-02-26 10:42:17 -0700

    Make Tables

---
+++ Mission08_Team0102_Metler/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_Team0102_Metler.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Mission08_Team0102_Metler/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mission08_Team0102_Metler.Models
+{
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+        [Required]
+        public string TaskName { get; set; }
+        public string? DueDate { get; set; }
+        [Required]
+        public int Quadrant { get; set; }
+        [ForeignKey("CategoryId")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public bool? Completed { get; set; }
+    }
+}
